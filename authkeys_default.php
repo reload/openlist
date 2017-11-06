9ae0a1461b54fbf6d1684ba95581ef580e032048
Martin Dahl <martin@b14.dk>
2017-11-06 13:28:31 +0100

    Authkeys seperate file and updates

---
+++ authkeys_default.php
@@ -0,0 +1,33 @@
+<?
+
+/* 
+ * @file
+ * Default authkey.
+ *
+ * Rename this file to authkey_defaults.php
+ *
+ * Insert auth key arrays below in the following format:
+ *
+ * '<authkey>' => array (
+ *   '<libcode>' = array(
+ *     'ip' => array('<ip adress>', ...) ,
+ *   )
+ * )
+ * 
+ *
+ */
+
+$authkeys = array(
+  // Test auth keys
+
+/*
+  'ab5492ae2735fa3b02ad98dbc1438580' => array(
+    '123456' => array(
+      'ip' => array(
+        array('000.000.000.000'),
+      ),
+    ),
+  ),
+*/
+
+);
